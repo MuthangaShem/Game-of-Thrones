28a660cea70e596db799e73e097962c1bd990d4f
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-03-02 17:04:07 +0300

    create an a array and a function that puts the random numbers in that array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+var randomNumber
+
 function rollDice(){
 	var die = document.getElementById('die');
-	var randomNumber = Math.ceil(Math.random()*6);
+	randomNumber = Math.ceil(Math.random()*6);
 	die.innerHTML = randomNumber;
 }
+
+var player1Array = [];
+
+function putInArray(){
+	if (randomNumber===1) {
+		alert("sorry, you rolled a one :(");
+	}
+	else{
+		player1Array.push(randomNumber);
+	}
+	console.log(player1Array);
+} 
